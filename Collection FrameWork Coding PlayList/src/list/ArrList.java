30ca56de023c6d44ed9e6d72a201926d1f8fb5c5
vijayprakashyadav018 <shaanahir33@gmail.com>
2025-05-06 16:07:25 +0530

    third update

--- Collection FrameWork Coding PlayList/src/list/ArrList.java
+++ Collection FrameWork Coding PlayList/src/list/ArrList.java
@@ -1,6 +1,8 @@
 package list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ArrList {
@@ -9,6 +11,7 @@ public class ArrList {
 
  
 		ArrayList<Integer> list = new ArrayList<>();
+		System.out.println(list.getClass().getName()); //gives the name of the clss
 		// we can write this also 
 		// List<Integer> arrlist = new ArrayList<>();
 		
@@ -17,7 +20,7 @@ public class ArrList {
 		list.add(5);
 		list.add(11);
 		System.out.println(list.get(2));
-		System.out.println(list.size());
+		System.out.println(list.size()); // gives number of elements in arraylist
 		for(int i = 0; i< list.size(); i++) {
 			System.out.println(list.get(i));
 		}
@@ -35,6 +38,31 @@ public class ArrList {
 		System.out.println(list.contains(88));
 		System.out.println(list);
 	
+		
+		List<String> list2 = Arrays.asList("Monday","Tuesday");
+		//list2.add("Wednesday"); (throw exception bcz it is fixed size)
+		
+		list2.set(1, "Wednesday"); // but we can replace and can also remove 
+		System.out.println(list2);
+		System.out.println(list2.getClass().getName());
+		
+		// But if we convert this array into arraylist than expception will not throw 
+		List<String> list5 = new ArrayList<>(list2);
+		list5.add("Friday");
+		System.out.println(list5);
+		
+		String[] array = {"Apple","Banana","Cherry"};
+		List<String> list3 = Arrays.asList(array);
+		System.out.println(list3.getClass().getName());
+		
+		List<Integer> list4 = List.of(1,2,55,11); //Returns an unmodifiable list containing four elements (Elements cannot be added, removed, or replaced)
+		
+		list.addAll(list4); // we can add list or array also 
+		System.out.println(list);
+		
+		Collections.sort(list); // no return type 
+		
+		System.out.println(list);
 	}
 
 }
