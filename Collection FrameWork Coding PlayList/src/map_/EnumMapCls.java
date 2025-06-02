8ab7718e1d852c6211465f6648f2bd52bc8d8ae8
vijayprakashyadav018 <shaanahir33@gmail.com>
2025-06-02 15:18:06 +0530

    update

--- Collection FrameWork Coding PlayList/src/map_/EnumMapCls.java
+++ Collection FrameWork Coding PlayList/src/map_/EnumMapCls.java
@@ -1,10 +1,22 @@
 package map_;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class EnumMapCls {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Map<Day, String> map = new EnumMap<>(Day.class);
+		map.put(Day.TUESDAY, "GYM");
+		map.put(Day.MONDAY, "WALK");
+		System.out.println(map);
+		
 	}
 
+	
+	enum Day {
+		MONDAY, TUESDAY, WEDNESDAY, THRUSDAY, FRIDAY, SATURDAY, SUNDAY
+	}
+	
 }
