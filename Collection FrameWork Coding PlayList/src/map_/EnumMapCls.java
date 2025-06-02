3c46fecf6f0ab9e37a6a7b9ddbd3294c20a326b3
vijayprakashyadav018 <shaanahir33@gmail.com>
2025-06-02 15:23:21 +0530

    update

--- Collection FrameWork Coding PlayList/src/map_/EnumMapCls.java
+++ Collection FrameWork Coding PlayList/src/map_/EnumMapCls.java
@@ -7,9 +7,16 @@ public class EnumMapCls {
 
 	public static void main(String[] args) {
 		
+		// it create array of size same as enum
+		// no hashing (hashcode)
+		// ordinal/index is used
+		// [_,"GYM",_,_,_,_,_]
+		// Faster then HashMap
 		Map<Day, String> map = new EnumMap<>(Day.class);
 		map.put(Day.TUESDAY, "GYM");
 		map.put(Day.MONDAY, "WALK");
+		String s = map.get(Day.TUESDAY);
+		System.out.println(s);
 		System.out.println(map);
 		
 	}
