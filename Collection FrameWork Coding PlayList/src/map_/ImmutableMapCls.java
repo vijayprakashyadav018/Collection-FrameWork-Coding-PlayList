4f572709493ca1f1cef4d8d409a79fd808c80459
vijayprakashyadav018 <shaanahir33@gmail.com>
2025-06-05 13:57:27 +0530

    updated

--- Collection FrameWork Coding PlayList/src/map_/ImmutableMapCls.java
+++ Collection FrameWork Coding PlayList/src/map_/ImmutableMapCls.java
@@ -17,7 +17,7 @@ public class ImmutableMapCls {
 		// map2.put("C", 3); // throws exception
 		Map<String, Integer> map3 = Map.of("Shubam", 98, "Vivek", 89); // here we can add 10 entries only.
 		// map3.put("Akshit", 88); // throws exception 
-		
+		//
 		Map<String, Integer> map4 = Map.ofEntries(Map.entry("akshit", 99), Map.entry("Vivek", 98)); // here we can add as many as entries as we like.
 
 	}
