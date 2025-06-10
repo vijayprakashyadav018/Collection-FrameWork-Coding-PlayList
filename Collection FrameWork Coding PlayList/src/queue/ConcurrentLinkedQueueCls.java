1d92bcc6fe2f8e24539ab0a18da3265a6fdbe41a
vijayprakashyadav018 <shaanahir33@gmail.com>
2025-06-10 13:23:02 +0530

    update

--- Collection FrameWork Coding PlayList/src/queue/ConcurrentLinkedQueueCls.java
+++ Collection FrameWork Coding PlayList/src/queue/ConcurrentLinkedQueueCls.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class ConcurrentLinkedQueueCls {
 
 	private static ConcurrentLinkedQueue<String> task = new ConcurrentLinkedQueue<>();
+	// here also compair and swap method used like concurrentHashMap
 	
 	public static void main(String[] args) {
 		
