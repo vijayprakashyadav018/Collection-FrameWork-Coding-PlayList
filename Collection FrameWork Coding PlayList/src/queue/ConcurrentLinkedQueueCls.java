c8889b0c42d5d12a5443b526e08d7318e5cfd3fa
vijayprakashyadav018 <shaanahir33@gmail.com>
2025-06-10 13:18:20 +0530

    update

---
+++ Collection FrameWork Coding PlayList/src/queue/ConcurrentLinkedQueueCls.java
@@ -0,0 +1,41 @@
+package queue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ConcurrentLinkedQueueCls {
+
+	private static ConcurrentLinkedQueue<String> task = new ConcurrentLinkedQueue<>();
+	
+	public static void main(String[] args) {
+		
+		Thread producer = new Thread(() ->{
+			while(true) {
+				try {
+					task.add("Task " + System.currentTimeMillis()); // yha lock nhi lgega 
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
+		Thread consumer = new Thread(() ->{
+			while(true) {
+				try {
+					String task1 = task.poll(); // noo lock and blocks here
+					System.out.println("Processing: " + task1);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		producer.start();
+		consumer.start();
+
+	}
+
+}
